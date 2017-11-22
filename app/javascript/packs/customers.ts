adc211582d0e2630856049841f2ea4b3727d2294
Laura <laura.peppiatt@btinternet.com>
2017-11-22 15:27:47 +0000

    Typehead Seach...

---
+++ app/javascript/packs/customers.ts
@@ -0,0 +1,119 @@
+import "hello_angular/polyfills";
+
+import { Component, NgModule    } from "@angular/core";
+import { BrowserModule          } from "@angular/platform-browser";
+import { FormsModule            } from "@angular/forms";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { Http,HttpModule        } from "@angular/http";
+
+
+var RESULTS = [
+  {
+    first_name: "Pat",
+    last_name: "Smith",
+    username: "psmith",
+    email: "pat.smith@somewhere.net",
+    created_at: "2016-02-05",  
+  },
+  {
+    first_name: "Patrick",
+    last_name: "Jones",
+    username: "pjpj",
+    email: "jones.p@business.net",
+    created_at: "2014-03-05",  
+  },
+  {
+    first_name: "Patricia",
+    last_name: "Benjamin",
+    username: "pattyb",
+    email: "benjie@aol.info",
+    created_at: "2016-01-02",  
+  },
+  {
+    first_name: "Patty",
+    last_name: "Patrickson",
+    username: "ppat",
+    email: "pppp@freemail.computer",
+    created_at: "2016-02-05",  
+  },
+  {
+    first_name: "Jane",
+    last_name: "Patrick",
+    username: "janesays",
+    email: "janep@company.net",
+    created_at: "2013-01-05",  
+  },
+];
+
+
+@Component({
+ selector: 'customer-search',
+ template: '\
+<header> \
+  <h1 class="h2">Customer Search</h1> \
+</header> \
+<section class="search-form"> \
+  <form> \
+      <label for="keywords" class="sr-only">Keywords></label> \
+      <input type="text" id="keywords" name="keywords" \
+            placeholder="First Name, Last Name, or Email Address"\
+            bind-ngModel="keywords" \
+            on-ngModelChange="search($event)" \
+            class="form-control input-lg">\
+      </form> \
+</section> \
+<section class="search-results"> \
+  <header> \
+    <h1 class="h3">Results</h1> \
+  </header> \
+  <ol class="list-group"> \
+    <li *ngFor="let customer of customers" \
+      class="list-group-item clearfix"> \
+      <h3 class="pull-right"> \
+        <small class="text-uppercase">Joined</small> \
+        {{customer.created_at}} \
+      </h3> \
+      <h2 class="h3"> \
+        {{customer.first_name}} {{customer.last_name}} \
+        <small>{{customer.username}}</small> \
+      </h2> \
+      <h4>{{customer.email}}</h4> \
+    </li> \
+  </ol> \
+</section> \
+  '
+})
+
+class CustomerSearchComponent {
+  customers;
+  keywords: string;
+  http: Http;
+  
+   constructor( http: Http) {
+     this.http      = http;
+     }
+   
+   search($event) {
+     this.keywords = $event;
+     if (self.keywords.length < 3) {
+       return;
+     } 
+    this.http.get("/customers.json?keywords=" + this.keywords) 
+        .subscribe( 
+          data => this.customers = data.json().customers, 
+          err => console.log(err) );                                
+   
+    }
+  }
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, HttpModule ],
+  declarations: [ CustomerSearchComponent ],
+  bootstrap:    [ CustomerSearchComponent ]
+})
+
+export class AppModule { }
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
